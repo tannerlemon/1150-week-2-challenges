29f19212b65ef96ce55f14afce02068bf71f0ffb
tannerlemon <tanrlemn@gmail.com>
2020-11-17 21:16:46 -0500

    Add project files.

---
+++ Week 2 Challenges/Challenges.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Week_2_Challenges
+{
+    [TestClass]
+    public class Challenges
+    {
+        [TestMethod]
+        public void Main()
+        {
+            Greeter hello = new Greeter();
+            hello.Hello("Tanner");
+            hello.Bye("Tanner");
+            hello.GreetingBasedOnTime("Tanner");
+        }
+    }
+
+    public class Greeter
+    {
+        public string Hello(string name)
+        {
+            Console.WriteLine("Hello " + name);
+            return "Hello" + name;
+        }
+
+        public string Bye(string name)
+        {
+            Console.WriteLine("See ya later " + name);
+            return "See ya later " + name;
+        }
+
+        public string GreetingBasedOnTime(string name)
+        {
+            DateTime now = DateTime.Now;
+            TimeSpan nowTimeSpan = now.TimeOfDay;
+
+            TimeSpan endOfMorning = new DateTime(2020, 11, 17, 11, 59, 0).TimeOfDay;
+            TimeSpan endOfAfternoon = new DateTime(2020, 11, 17, 15, 59, 0).TimeOfDay;
+            TimeSpan endOfEvening = new DateTime(2020, 11, 17, 19, 59, 0).TimeOfDay;
+            TimeSpan endOfNight = new DateTime(2020, 11, 17, 3, 59, 0).TimeOfDay;
+
+
+            if (nowTimeSpan <= endOfNight || nowTimeSpan > endOfEvening)
+            {
+                Console.WriteLine("Good Night! " + name);
+                return "Good Night!" + name;
+            }
+            else if (nowTimeSpan <= endOfMorning)
+            {
+                Console.WriteLine("Good Morning! " + name);
+                return "Good Morning!" + name;
+            }
+            else if (nowTimeSpan <= endOfAfternoon)
+            {
+                Console.WriteLine("Good Afternoon! " + name);
+                return "Good Afternoon!" + name;
+            }
+            else
+            {
+                Console.WriteLine("Good Evening! " + name);
+                return "Good Evening!" + name;
+            }
+        }
+    }
+}
---
+++ Week 2 Challenges/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Week 2 Challenges")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Week 2 Challenges")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("efdc52a4-5ba1-4452-98e1-59ed0db064db")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
